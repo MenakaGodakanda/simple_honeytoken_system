8dbd364523fc4a8a96d2be5a42f57cac04dad19f
Menaka Godakanda <156875412+MenakaGodakanda@users.noreply.github.com>
2024-07-08 23:21:35 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,20 +4,25 @@ import datetime
 LOG_FILE = 'logs/honeyfile_log.txt'
 HONEYFILE = 'honeyfile.txt'
 
-def log_interaction(event):
+def log_interaction(event, file):
     with open(LOG_FILE, 'a') as log:
-        log.write(f'{datetime.datetime.now()}: {event}\n')
+        log.write(f'{datetime.datetime.now()}: {event} {file}\n')
 
 def monitor_honeyfile():
     process = subprocess.Popen(
-        ['inotifywait', '-m', HONEYFILE, '--format', '%e', '--event', 'open,modify,delete'],
+        ['inotifywait', '-m', '.', '--format', '%e %w%f', '--event', 'open,modify,delete,create'],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
 
     for line in process.stdout:
-        event = line.decode('utf-8').strip()
-        log_interaction(event)
+        event, filepath = line.decode('utf-8').strip().split(' ', 1)
+        if filepath == f"./{HONEYFILE}":
+            log_interaction(event, filepath)
+
+    for line in process.stderr:
+        error = line.decode('utf-8').strip()
+        log_interaction(f"ERROR: {error}", '')
 
 if __name__ == "__main__":
     monitor_honeyfile()
